10cacafac39c6f5726e38c2a700dd600bbb39afd
nakul-NRA <117747503+nakul-NRA@users.noreply.github.com>
2022-11-08 20:45:02 +0530

    Add files via upload

---
+++ definitions.h
@@ -0,0 +1,22 @@
+#pragma once
+
+extern int number_of_nodes;
+
+struct Tree
+{
+    int data;
+    int number_of_children;
+    struct Tree** children; 
+}typedef tree;
+
+struct Node
+{
+    int val;
+    char pr;
+}typedef node;
+
+struct ldsnode
+{
+    int posn;
+    char bn;
+}typedef ldsnode;
